c24317ac9be85f51e6a9aca2a26fe9ad7ed1e811
jtuntalan <38850519+jtuntalan@users.noreply.github.com>
2019-04-20 16:35:18 -0700

    Update xed.cpp

--- xed.cpp
+++ xed.cpp
@@ -1,3 +1,12 @@
+/*
+*Student names: Jose Untalan, Daniel Castro, Edgar Delgado
+*Class accounts: cssc0462, cssc0433, cssc0444
+*Instructor: Guy Leonard
+*Class: CS530, Spring 2019
+*Assignment #2: SIC/XE Disassembler
+*This file: README.txt
+*/
+
 #include <iomanip>
 
 ifsteam objectStream;
@@ -6,9 +15,18 @@ ofstream sicStream;
 ofstream lisStream;
 
 void xed::fileOpener(char *objectFile) {
+	string symbolFile = objectFile;
+	symbolFile.erase(symbolFile.find_last_of('.'));
+	symbolFile.append(".sym");
+	symbolStream.open(symbolFile.c_str());
+	if (symbolSTream.is_open()==false) {
+		cout<<"Error: The .sym file cannot be found."<<endl;
+		exit(0);
+	}
+	
 	objectStream.open(objectFile);
 	if (objectStream.is_open()==false) {
-		cout<< "Error: The .obj file is not found." << endl;
+		cout<< "Error: The .obj file cannot be not found." << endl;
 		exit(0);
 	}
 }
